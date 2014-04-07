20b78b63f5e9a6191b43619164ff863a935b5311
yanniks <kontakt@yanniks.de>
2014-04-07 18:22:20 +0200

    initial CyanDream commit
    Change-Id: I1b5aede909c3da13409deebd9b86de14dc9957d3

--- cd.mk
+++ cd.mk
@@ -1,22 +1,22 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/cyandream/config/cdma.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyandream/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/ls980/ls980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ls980
-PRODUCT_NAME := cm_ls980
+PRODUCT_NAME := cd_ls980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-LS980
 PRODUCT_MANUFACTURER := lge
